c638de8f7df500707b1e55ea7c9d5e5e988076be
Leonardo Neves <lneves@protonmail.com>
2017-06-17 00:33:36 +0200

    Reducer Script
    Create N-Grams with MapReduce - Mapper Sketch

---
+++ Reducer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import fileinput
+from collections import Counter, defaultdict
+
+
+ngram_counts = defaultdict(Counter)
+for line in fileinput.input():
+    ngram, count = line.split('\t', 1)
+    ngram, count = tuple(ngram.split(' ')), int(count)
+    length = len(ngram)
+    ngram_counts[length][ngram] += count
+
+for length in ngram_counts:
+    for ngram, count in ngram_counts[length].items():
+        print ('{}\t{}'.format(' '.join(ngram), count))
