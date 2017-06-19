c9b48f4ce3f5792edd00fdd080d2bc98f7a333d0
Leonardo Neves <lneves@protonmail.com>
2017-06-19 04:01:38 +0200

    Reducer Update

--- Reducer.py
+++ Reducer.py
@@ -2,7 +2,6 @@
 import fileinput
 from collections import Counter, defaultdict
 
-
 ngram_counts = defaultdict(Counter)
 for line in fileinput.input():
     ngram, count = line.split('\t', 1)
@@ -13,3 +12,6 @@ for line in fileinput.input():
 for length in ngram_counts:
     for ngram, count in ngram_counts[length].items():
         print ('{}\t{}'.format(' '.join(ngram), count))
+
+
+# echo "hellow how are you" | /home/lneves/Dropbox/Data\ Science/Local\ MapReduce/local-mapreduce/lmr 5m 8 '/home/lneves/Softwares/Anaconda/Python3/bin/python3.6 Mapper.py' '/home/lneves/Softwares/Anaconda/Python3/bin/python3.6 Reducer.py' ./out4
