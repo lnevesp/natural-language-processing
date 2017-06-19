c49602b002937fd70d9fb6e20962b60bc7a2fb24
Leonardo Neves <lneves@protonmail.com>
2017-06-19 04:01:15 +0200

    Mapper Update

--- Mapper.py
+++ Mapper.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
-import re
-import fileinput
+
 from collections import Counter
+import pickle
 
-def tokens(str1): return re.findall('[a-z]+', str1.lower())
 
-def to_ngrams( words, length):
-    return zip(*[words[i:] for i in range(length)])  
+def createNgram( words, length):
+    return zip(*[words[i:] for i in range(length)])
 
-for line in fileinput.input():
-    words = tokens(line)
+with open('../data/Tokens.pickle', "rb") as file:  # Unpickling
+    TokensData = pickle.load(file)
+
+for line in TokensData:
     for n in range(1, 6):
-        ngrams_counts = Counter(to_ngrams(words, n))
+        ngrams_counts = Counter(createNgram(line, n))
         for ngram, count in ngrams_counts.items():
             print ('{}\t{}'.format(' '.join(ngram), count))
