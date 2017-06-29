e5f71c3e42571d5c1f07c3184040fd87f7083997
Leonardo Neves <lneves@protonmail.com>
2017-06-29 21:19:50 +0200

    Upgrade CleanData.py
    Using Formats.py and saving the log

--- CleanData.py
+++ CleanData.py
@@ -6,71 +6,75 @@ import html
 import nltk
 import sys
 import os.path
+from Formats import TimeFormats
+from collections import defaultdict
 
 
 class CleanCorpus:
 
-    def __init__(self, RawCorpus = "../data/RawCorpus.txt"):
-        self.color01 = "\033[92m" # Green
-        self.color02 = "\033[93m"  # Yellow
-        startCleanData = datetime.datetime.now().time().strftime('%H:%M:%S')
-        print(self.color01 + "\n>>> " + "\033[0m" +
-              "Starting CleanData.py - " + self.color01 + startCleanData + "\033[0m")
+    def __init__(self, RawCorpus = "../data/RawCorpus.txt", infoLog = defaultdict()):
+        self.color01 = "\033[92m"  # Green
+        self.infoLog = infoLog
+
+        StartCleanScript = datetime.datetime.now().time().strftime('%H:%M:%S')
+        TimeFormats.StartScript(self, time=StartCleanScript, phrase="Starting CleanData.py")
 
         if os.path.isfile("../data/Tokens.pickle") != 1:
 
             self.read_corpora(RawCorpus)
 
-            finishCreateTokens = datetime.datetime.now().time().strftime('%H:%M:%S')
-            TimeElapse = (datetime.datetime.strptime(finishCreateTokens, '%H:%M:%S') -
-                          datetime.datetime.strptime(self.startCreateTokens, '%H:%M:%S'))
-            print(" - Time Elapse: " + self.color01 + str(TimeElapse) + "\033[0m")
+            StopCleanLines = datetime.datetime.now().time().strftime('%H:%M:%S')
+            self.infoLog['TECleanLines'] = (datetime.datetime.strptime(StopCleanLines, '%H:%M:%S') -
+                                            datetime.datetime.strptime(StartCleanScript, '%H:%M:%S'))
+            TimeFormats.timeElapse2(self, self.infoLog['TECleanLines'])
 
-            startWriteData = datetime.datetime.now().time().strftime('%H:%M:%S')
-            print(self.color01 + ">>>  " + startWriteData + "\033[0m" +
-                  " Writing Tokens File", end='', flush=True)
+            StartWriteTokens = datetime.datetime.now().time().strftime('%H:%M:%S')
+            TimeFormats.timeElapse1(self, time=StartWriteTokens, phrase="Writing Tokens File")
+
             # If the tokens were created then save then into Tokens.pickle file
             if self.Tokens:
                 with open('../data/Tokens.pickle', 'wb') as file:
                     pickle.dump(self.Tokens, file, pickle.HIGHEST_PROTOCOL)
-            finishWriteData = datetime.datetime.now().time().strftime('%H:%M:%S')
-            TimeElapse = (datetime.datetime.strptime(finishWriteData, '%H:%M:%S') -
-                          datetime.datetime.strptime(startWriteData, '%H:%M:%S'))
-            print(self.color01 + " | " + "\033[0m" + "Time Elapse: " + self.color01 + str(TimeElapse) + "\033[0m")
-        else:
-            print(self.color01 + ">>>  " + datetime.datetime.now().time().strftime('%H:%M:%S') + "\033[0m" +
-                  " Tokens already created")
+            # Print time elapse
+            StopWriteTokens = datetime.datetime.now().time().strftime('%H:%M:%S')
+            self.infoLog['TEWriteTokens'] = (datetime.datetime.strptime(StopWriteTokens, '%H:%M:%S') -
+                                          datetime.datetime.strptime(StartWriteTokens, '%H:%M:%S'))
+            TimeFormats.timeElapse2(self, TimeElapse=self.infoLog['TEWriteTokens'])
 
-        finishCreateCorpus = datetime.datetime.now().time().strftime('%H:%M:%S')
-        TimeElapse = (datetime.datetime.strptime(finishCreateCorpus, '%H:%M:%S') -
-                      datetime.datetime.strptime(startCleanData, '%H:%M:%S'))
-        print(self.color01 + ">>> " + "\033[0m" + "CleanData.py Finished" + self.color01 + " | " + "\033[0m" +
-              " Time Elapse: " + self.color01 + str(TimeElapse) + "\033[0m")
+        else:
+            TimeFormats.NormalMessage(self, phrase="Tokens already created")
+            self.infoLog['TEWriteTokens'] = datetime.timedelta(0)
+
+        # Print Final Time
+        StopCleanScript = datetime.datetime.now().time().strftime('%H:%M:%S')
+        self.infoLog['TECleanScript'] = (datetime.datetime.strptime(StopCleanScript, '%H:%M:%S') -
+                                         datetime.datetime.strptime(StartCleanScript, '%H:%M:%S'))
+        TimeFormats.StopScript(self, TimeElapse=self.infoLog['TECleanScript'], phrase="CleanData.py Finished")
 
 
     def createTokens(self, line, vocab):
-        words_vector = [] # Creates an empty list
+        words_vector = []  # Creates an empty list
         line = html.unescape(line)  # Removes HTML or XML character references and entities.
         line = [x for x in map(str.strip, line.split('.')) if x]  # Splits  Lines on Period
         line = [x.lower() for x in line]  # Convert to lowercase
         for Sentence in line:
-            Sentence = Sentence.strip() # Deletes the /n on each sentence
+            Sentence = Sentence.strip()  # Deletes the /n on each sentence
             Sentence = ''.join(i for i in Sentence if not i.isdigit())  # Remove numbers
             words = word_tokenize(Sentence)  # Convert lines into word vector (tokenizing)
-            words = self.unusual_words(text=words, vocab = vocab) # Clean the vocabulary
-            words = self.removePunctuation(tokens=words) # Removes punctuation
-            words_vector.append(words) # Appends the tokens
+            words = self.unusual_words(text=words, vocab=vocab)  # Clean the vocabulary
+            words = self.removePunctuation(tokens=words)  # Removes punctuation
+            words_vector.append(words)  # Appends the tokens
         return words_vector
 
     def unusual_words(self, text, vocab):
-        text_vocab = set(w.lower() for w in text if w.isalpha()) # Create a set with the sentence's tokens
-        unusual = text_vocab - vocab # Save the words that don't belong to the "english vocabulary"
-        Text = text_vocab - unusual # save the words that belong "english vocabulary"
+        text_vocab = set(w.lower() for w in text if w.isalpha())  # Create a set with the sentence's tokens
+        unusual = text_vocab - vocab  # Save the words that don't belong to the "english vocabulary"
+        Text = text_vocab - unusual  # save the words that belong "english vocabulary"
         return Text
 
     # Remove Punctuation
     def removePunctuation(self, tokens):
-        regex = re.compile('[^a-zA-Z]') #
+        regex = re.compile('[^a-zA-Z]')
 
         words_vector_RP = []
         for token in tokens:
@@ -82,22 +86,25 @@ class CleanCorpus:
 
     """Read the Raw Corpus, returns all sentences tokenized and cleaned"""
     def read_corpora(self, RawCorpus):
-        startReading = datetime.datetime.now().time().strftime('%H:%M:%S')
-        print(self.color01 + ">>>  " + startReading + "\033[0m" +
-              " Reading Corpus", end='', flush=True)
+        # Print start download time
+        startReadingCorpus = datetime.datetime.now().time().strftime('%H:%M:%S')
+        TimeFormats.timeElapse1(self, time=startReadingCorpus, phrase="Reading Corpus")
+
         rawCorpus = open(RawCorpus)
         rawCorpus = rawCorpus.readlines()
+
         # Print time elapse
-        finishReading = datetime.datetime.now().time().strftime('%H:%M:%S')
-        TimeElapse = (datetime.datetime.strptime(finishReading, '%H:%M:%S') -
-                      datetime.datetime.strptime(startReading, '%H:%M:%S'))
-        print(" - Time Elapse: " + self.color01 + str(TimeElapse) + "\033[0m")
+        stopReadingCorpus = datetime.datetime.now().time().strftime('%H:%M:%S')
+        self.infoLog['TEReadCorpus'] = (datetime.datetime.strptime(stopReadingCorpus, '%H:%M:%S') -
+                                      datetime.datetime.strptime(startReadingCorpus, '%H:%M:%S'))
+        TimeFormats.timeElapse2(self, TimeElapse=self.infoLog['TEReadCorpus'])
 
 
         """Using nltk corpora to built a large corpus of english words"""
+
         startVocabulary = datetime.datetime.now().time().strftime('%H:%M:%S')
-        print(self.color01 + ">>>  " + startVocabulary + "\033[0m" +
-              " Creating English Vocabulary Set", end='', flush=True)
+        TimeFormats.timeElapse1(self, time=startVocabulary, phrase="Creating English Vocabulary Set")
+
         english_vocab = set(w.lower() for w in nltk.corpus.brown.words())
         english_vocab = english_vocab.union(set(w.lower() for w in nltk.corpus.reuters.words()))
         english_vocab = english_vocab.union(set(w.lower() for w in nltk.corpus.words.words()))
@@ -112,27 +119,26 @@ class CleanCorpus:
         english_vocab = english_vocab.union(set(w.lower() for w in nltk.corpus.treebank.words()))
         english_vocab = english_vocab.union(set(w.lower() for w in nltk.corpus.stopwords.words()))
         english_vocab = english_vocab.union(set(w.lower() for w in nltk.corpus.state_union.words()))
+
         # Print time elapse
-        finishVocabulary = datetime.datetime.now().time().strftime('%H:%M:%S')
-        TimeElapse = (datetime.datetime.strptime(finishVocabulary, '%H:%M:%S') -
-                      datetime.datetime.strptime(startVocabulary, '%H:%M:%S'))
-        print(" - Time Elapse: " + self.color01 + str(TimeElapse) + "\033[0m")
+        stopVocabulary = datetime.datetime.now().time().strftime('%H:%M:%S')
+        self.infoLog['TEVocabulary'] = (datetime.datetime.strptime(stopVocabulary, '%H:%M:%S') -
+                                        datetime.datetime.strptime(startVocabulary, '%H:%M:%S'))
+        TimeFormats.timeElapse2(self, TimeElapse=self.infoLog['TEVocabulary'])
 
-        self.startCreateTokens = datetime.datetime.now().time().strftime('%H:%M:%S')
-        print(self.color01 + ">>>  " + self.startCreateTokens + "\033[0m" +
-              " Cleaning Data", end='', flush=True)
 
+        self.infoLog['StartCleanLines'] = datetime.datetime.now().time().strftime('%H:%M:%S')
         self.Tokens = []
         i = 0
         totalLines = len(rawCorpus)
         for line in rawCorpus:
             i += 1
-            sys.stdout.write("\r" + self.color01 + ">>>  " + self.startCreateTokens + "\033[0m" +
+            sys.stdout.write("\r" + self.color01 + ">>>  " + self.infoLog['StartCleanLines'] + "\033[0m" +
                              " Cleaning Data" + self.color01 + " | " + "\033[0m" + "Line: " +
                              self.color01 + str(i) + "/" + str(totalLines) +
                              " (" + str(round((i/totalLines)*100,2)) + "%)" + "\033[0m")
             sys.stdout.flush()
-            LineToken = self.createTokens(line = line, vocab = english_vocab)
+            LineToken = self.createTokens(line=line, vocab=english_vocab)
             if LineToken:
                 for line in LineToken:
                     self.Tokens.append(line)
