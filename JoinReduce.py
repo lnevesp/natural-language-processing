74853f098ad361447b766b1e33b68648bc9b4d00
Leonardo Neves <lneves@protonmail.com>
2017-07-02 20:06:56 +0200

    Introducing JoinReduce.py

---
+++ JoinReduce.py
@@ -0,0 +1,19 @@
+import glob
+
+
+ReduceOutput = glob.glob('../data/MapReduce/*')
+
+DataPath = '../data/'
+
+with open(DataPath + "FullNgrams.txt", 'w') as outfile:
+    for fname in ReduceOutput:
+        with open(fname) as infile:
+            for line in infile:
+                outfile.write(line)
+
+# class JoinOutput:
+#     def reading(self):
+#         with open('../MapReduce/reducer-00.txt', 'r') as f:
+#             s = f.read()
+#             self.whip = ast.literal_eval(s)
+
