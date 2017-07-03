25b90fe6c29d7bb17aeed39508768141e3a5eb08
Leonardo Neves <lneves@protonmail.com>
2017-07-03 22:26:51 +0200

    Reducer.py Upgrade
    Comma Separated Input/Output

--- Reducer.py
+++ Reducer.py
@@ -3,15 +3,17 @@ import fileinput
 from collections import Counter, defaultdict
 
 ngram_counts = defaultdict(Counter)
+
 for line in fileinput.input():
+    line = line.strip()
     ngram, count = line.split('\t', 1)
-    ngram, count = tuple(ngram.split(' ')), int(count)
-    length = len(ngram)
+    length = len(ngram.split(','))
+    ngram, count = tuple(ngram.split(',')), int(count)
     ngram_counts[length][ngram] += count
 
 for length in ngram_counts:
     for ngram, count in ngram_counts[length].items():
-        print ('{}\t{}'.format(' '.join(ngram), count))
+        print('{},{}'.format(','.join(ngram), count))
 
 
 # // echo "Word Prediction" | /home/lneves/Dropbox/Data\ Science/Local\ MapReduce/local-mapreduce/lmr 5m 8 'python Mapper.py' 'python Reducer.py' ../data/MapReduceOutput_00001
