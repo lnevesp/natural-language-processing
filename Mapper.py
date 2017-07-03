2ff6230e90063c0ece150f5ee2dae587c58b49a1
Leonardo Neves <lneves@protonmail.com>
2017-07-03 22:26:21 +0200

    Mapper.py Upgrade
    Comma Separated Output

--- Mapper.py
+++ Mapper.py
@@ -2,7 +2,10 @@
 
 from collections import Counter
 import pickle
+import datetime
 
+# time = datetime.datetime.now().time().strftime('%H:%M:%S')
+# print("Starting Mapper" + time)
 
 def createNgram( words, length):
     return zip(*[words[i:] for i in range(length)])
@@ -10,8 +13,9 @@ def createNgram( words, length):
 with open('../data/SampleTokens.pickle', "rb") as file:  # Unpickling
     TokensData = pickle.load(file)
 
+# print("Starting MAPREDUCE! " + time)
 for line in TokensData:
     for n in range(1, 6):
         ngrams_counts = Counter(createNgram(line, n))
         for ngram, count in ngrams_counts.items():
-            print ('{}\t{}'.format(' '.join(ngram), count))
+            print('{}\t{}'.format(','.join(ngram), count))
